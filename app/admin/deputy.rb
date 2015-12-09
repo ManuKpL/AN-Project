9c9526a1a4e4bd71afdd89f80d974d6d9cad9a53
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-12-09 01:10:53 +0100

    started AA configuration

---
+++ app/admin/deputy.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Deputy do
+  permit_params :civ, :firstname, :lastname, :birthday, :birthdep, :original_tag, :job_id, :screen_name
+
+  index do
+    selectable_column
+    column :id
+    column :civ
+    column :firstname
+    column :lastname
+    column :original_tag
+    column :screen_name
+    column :job
+    actions
+  end
+end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,7 @@
 class Job < ActiveRecord::Base
   has_many :deputies
+
+  def name
+    label
+  end
 end
