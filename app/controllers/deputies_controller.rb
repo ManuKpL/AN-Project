d8fbae18b1655c35d13c54b7ab1e27bef0069770
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-12-28 22:53:58 +0100

    modal navigation functionment

--- app/controllers/deputies_controller.rb
+++ app/controllers/deputies_controller.rb
@@ -1,13 +1,13 @@
 class DeputiesController < ApplicationController
-  before_action :set_deputy, :set_previous_and_next
-  before_action :set_group, only: :index
-  helper_method :check_status
+  before_action :set_group, :set_deputy
+  before_action :set_previous_and_next, only: :show
+  helper_method :check_status, :set_next, :set_previous
 
   def show
   end
 
   def index
-    redirect_to root_path if @deputies.empty?
+    redirect_to root_path if @deputies.length == 577
   end
 
   private
@@ -16,34 +16,38 @@ class DeputiesController < ApplicationController
     if params[:id]
       @deputy = Deputy.find(params[:id].to_i)
     else
-      @deputy = Deputy.first
+      @deputy = @deputies.first
     end
   end
 
   def set_previous_and_next
-    ids_list = []
-    Deputy.order(:lastname).each do |e|
-      ids_list << e.id
-    end
-    @previous_id = Deputy.find(ids_list[ids_list.find_index(@deputy.id) - 1])
-    if ids_list[ids_list.find_index(@deputy.id) + 1]
-      @next_id = Deputy.find(ids_list[ids_list.find_index(@deputy.id) + 1])
-    else
-      @next_id = Deputy.find(ids_list.first)
-    end
+    @previous_id = set_previous(@deputy)
+    @next_id = set_next(@deputy)
   end
 
   def set_group
-    if params[:search].nil?
-      @deputies = []
+    if params[:search].nil? || Group.find_by(sigle: params[:search]).nil?
+      @deputies = Deputy.order(:lastname)
     elsif params[:search].length == 1
       @deputies = Deputy.where('lastname LIKE ?', "#{params[:search].capitalize}%").order(:lastname)
-    elsif Group.find_by(sigle: params[:search]).nil?
-      @deputies = []
     else
       @deputies = Deputy.where(group_id: Group.find_by(sigle: params[:search]).id).order(:lastname)
     end
     @groups = Group.order(:sigle)
+  end
+
+  def set_previous(deputy)
+    ids_list = @deputies.map(&:id)
+    return Deputy.find(ids_list[ids_list.find_index(deputy.id) - 1])
+  end
+
+  def set_next(deputy)
+    ids_list = @deputies.map(&:id)
+    if ids_list[ids_list.find_index(deputy.id) + 1]
+      return Deputy.find(ids_list[ids_list.find_index(deputy.id) + 1])
+    else
+      return Deputy.find(ids_list.first)
+    end
   end
 
   def check_status(element)
