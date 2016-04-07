c3dbbd88c1a816977f3100d4e4904d4185779ec6
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-04-07 12:13:23 +0200

    added last method as helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :check_status, :is_current_page?
+  helper_method :check_status, :is_current_page?, :ordinalize_FR
 
   private
 
