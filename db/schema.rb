d27591ade15c1e554dcf1d543e005a61c6ebf6b5
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-05-22 23:27:03 +0200

    added and seeded twitter name attribute to deputies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522164524) do
+ActiveRecord::Schema.define(version: 20160522212140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20160522164524) do
     t.string   "picture"
     t.string   "profile_picture"
     t.string   "profile_banner"
+    t.string   "twitter_name"
   end
 
   add_index "deputies", ["group_id"], name: "index_deputies_on_group_id", using: :btree
